12acc45d4efc19520bc993f7f49ffa3aaa7f3cb8
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-09-01 18:17:14 +0300

    add gallery section

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,11 +4,13 @@ import Layout from "@/layout/Layout";
 
 import Header from "@/components/header"
 import Contact from "@/components/contact"
+import Gallery from "@/components/Gallery";
 
 const index = () => {
   return (
     <Layout>
       <Header />
+      <Gallery />
       <Contact />
     </Layout>
   )
